71cddf474dc73c612eb024d2495ced721d3b161c
Ian Scott <scottae1425@gmail.com>
2024-10-25 14:42:27 -0600

    My test python

---
+++ python-project/test.py
@@ -0,0 +1,6 @@
+
+golf_course_road_map='Golf courses I have worked at'
+course_names = ['Firethorn', 'Whitetail Club', 'The Quarry', 'Columbine CC']
+
+courses_worked_at= golf_course_road_map + " " + course_names
+print(courses_worked_at)
