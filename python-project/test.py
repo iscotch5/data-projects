417ce03c0f31518e769f2dd6b6ea562eb692cef6
iscotch5 <scottae1425@gmail.com>
2024-11-21 07:59:57 -0700

    Updated Spacing

--- python-project/test.py
+++ python-project/test.py
@@ -2,6 +2,6 @@
 golf_course_road_map='Golf courses I have worked at'
 course_name = 'Firethorn'
 
-courses_worked_at= golf_course_road_map + " " + ":" + course_name
+courses_worked_at= golf_course_road_map + ":" + " " + course_name
 print(courses_worked_at)
 print(course_name)
