2d0e49d26fd45994e51f6e6df01f6bd6e471107b
iscotch5 <scottae1425@gmail.com>
2025-09-04 08:38:42 -0600

    Test pull request.

--- python-project/test.py
+++ python-project/test.py
@@ -1,8 +1,9 @@
 
 golf_course_road_map='Golf courses I have worked at'
 course_name = 'Firethorn'
-
+work_place = 'FareHarbor'
 courses_worked_at= golf_course_road_map + ":" + " " + course_name
 print(courses_worked_at)
 print(course_name)
 print(golf_course_road_map)
+print(work_place)
