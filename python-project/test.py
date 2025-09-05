dad7c9f1261852a33d0e1a8dc793b02458ad5e93
iscotch5 <scottae1425@gmail.com>
2025-09-05 08:10:02 -0600

    Testing the use of F.

--- python-project/test.py
+++ python-project/test.py
@@ -1,9 +1,11 @@
 
 golf_course_road_map='Golf courses I have worked at'
-course_name = 'Firethorn'
-work_place = 'FareHarbor'
+course_name = "Firethorn"
+work_place = "FareHarbor"
 courses_worked_at= golf_course_road_map + ":" + " " + course_name
 print(courses_worked_at)
 print(course_name)
 print(golf_course_road_map)
 print(work_place)
+best_course = (f"{course_name} is the number {1} course in Nebraska")
+print(best_course)
